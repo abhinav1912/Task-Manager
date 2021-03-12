fa032ae43ec19cbe016a3291f4a869472fb5c7c3
abhinav1912 <abhinavmathur1912@gmail.com>
2021-03-12 10:42:22 +0530

    added mongoose models

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
 
+const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1/task-manager-api', {
     useNewUrlParser: true,
@@ -7,20 +8,74 @@ mongoose.connect('mongodb://127.0.0.1/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value){
+            if (value < 0){
+                throw new Error("Age must be > 0")
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                throw new Error("Invalid email!")
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value){
+            if (value.toLowerCase().includes("password")){
+                throw new Error("Must not contain the word `password`")
+            }
+        }
     }
 })
 
-const me = new User({
-    name: "A",
-    age: 21
+// const me = new User({
+//     name: "A",
+//     age: 21,
+//     email: "yo@g.com",
+//     password: "123456"
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
-me.save().then(() => {
-    console.log(me)
+const task = new Task({
+    description: "sample task"
+})
+
+task.save().then(() => {
+    console.log(task)
 }).catch((error) => {
     console.log(error)
 })
